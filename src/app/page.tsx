5ae9923f85888bdbe7c9ce759c88dab86a775669
lewisjhon <happin0202@naver.com>
2024-02-11 16:24:40 +0900

    로그 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,9 @@ const ServerPage = () => {
                 backgroundSize: 'cover', // 배경 이미지를 화면에 맞게 늘림
                 height: '100vh', // 브라우저 높이로 설정
             }}>
-                
+            <div className="text-white">
+                {start} / {current} / { toLocaleDate(dayjs(), "YYYY-MM-DD HH:mm:ss") } / {gap}
+            </div>
             <div className="bg-white bg-opacity-40 h-screen flex justify-center items-center">
                 <section className="flex-col text-center !font-daeam">
                     <p className="text-xl font-bold">{wisdom.data[getIndex(wisdom.data.length)].phrase}</p>
