fd92f682d69d2702a89a920b2aa6a714ca42401c
전강태 <lewisjhon@com2us.com>
2024-02-13 12:04:34 +0900

    테스트 코드 적용.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,17 @@ import {toLocaleDate} from "@/utils/date";
 import Image from "next/image";
 import Header from "@/components/Header";
 
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.locale("ko");
+// const timeZone = "Asia/Seoul"; // 한국 타임존
+const timeZone =
+    Intl.DateTimeFormat().resolvedOptions().timeZone || "Asia/Seoul";
+
 const ServerPage = () => {
     const wisdom = require('../../public/data/ko/wisdom.json') as DataModel<WisdomModel>;
     const background = require('../../public/data/background.json') as DataModel<BackgroundModel>;
@@ -17,6 +28,8 @@ const ServerPage = () => {
     const start = process.env.NEXT_PUBLIC_START_DATE;
     const current = toLocaleDate(dayjs(), "YYYY-MM-DD");
     const gap = dayjs(current).date() - dayjs(start).date();
+
+    const test = dayjs.utc(new Date()).tz(timeZone).format("YYYY-MM-DD hh:mm:ss");
 
     const getIndex = (length: number) => {
         const index = gap % length;
@@ -31,11 +44,11 @@ const ServerPage = () => {
                 backgroundSize: 'cover', // 배경 이미지를 화면에 맞게 늘림
                 height: '100vh', // 브라우저 높이로 설정
             }}>
-            {/* <div className="text-white">
-                {start} / {current} / { toLocaleDate(dayjs(), "YYYY-MM-DD HH:mm:ss") } / {gap} 
-            </div> */}
+            <div className="text-white">
+                { test }
+            </div>
             <div className="bg-white bg-opacity-40 h-screen flex justify-center items-center">
-                <section className="flex-col text-center !font-daeam">
+                <section className="flex-col text-center !font-daeam w-[94%] md:w-[40rem]">
                     <p className="text-xl font-bold">{wisdom.data[getIndex(wisdom.data.length)].phrase}</p>
                     <p className="text-sm">- {wisdom.data[getIndex(wisdom.data.length)].author} -</p>
                 </section>
