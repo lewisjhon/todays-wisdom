6b9a193c41a213db2aa206d449e196bf08bcd705
전강태 <lewisjhon@com2us.com>
2024-02-14 17:52:14 +0900

    footer 문구 변경.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ const Footer = () => {
     return (
         <div className="absolute bottom-auto flex h-[2rem] w-screen items-center justify-center bg-gray-700 bg-opacity-70 text-xs text-white">
             <p>
+                This page was created by{" "}
                 <a href="mailto:happin0202@naver.com">
                     <u>Lewis J.</u>
-                </a>{" "}
-                All Rights Reserved.
+                </a>
             </p>
         </div>
     );
