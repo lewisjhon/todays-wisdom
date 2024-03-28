94d8d4df07280e491d1f8c0f9335c29b4ff6656d
전강태 <lewisjhon@com2us.com>
2024-03-28 17:39:13 +0900

    mobile ui 조정

--- src/components/Introduce.tsx
+++ src/components/Introduce.tsx
@@ -38,7 +38,7 @@ const Introduce = ({ title = "" }: { title?: string }) => {
             <AnimatePresence>
                 {isVisible && (
                     <motion.div
-                        className="box fixed left-1/2 top-1/2 h-[28rem] w-[31rem] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl bg-gray-700 p-10 text-start text-white"
+                        className="box fixed left-1/2 top-1/2 h-[28rem] w-[22rem] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl bg-gray-700 p-10 text-start text-white md:w-[31rem]"
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
@@ -57,7 +57,7 @@ const Introduce = ({ title = "" }: { title?: string }) => {
                             </motion.li>
                             <motion.li variants={item}>
                                 삶이 힘들고 지친 모든 분들에게 조금이나마
-                                지혜(Wisdom)를 드리기 위해 제작되었습니다.
+                                지혜(Wisdom)를 드리기 위해 제작 되었습니다.
                             </motion.li>
                             <motion.li variants={item}>
                                 매일매일 인생에 도움이 되는 명언을 보여줍니다.
